fc4b3f36c7301c502cb5daa48000a0f70605a779
Shashank D Urs <shashankdurs77@gmail.com>
2024-11-21 15:39:08 +0530

    Add files via upload

---
+++ Urs/app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template
+
+
+flask_app = Flask(__name__)
+
+
+@flask_app.route('/')
+def index():
+    dic = {"name":"Shashank", "mobel":12345678910}
+    return render_template('index.html',context=dic)
+
+
+@flask_app.route('/about-us')
+def about_us():
+    return render_template('about.html')
+
+
+@flask_app.route('/contact-us')
+def contact_us():
+    return render_template('contact.html')
+
+
+if __name__ == '__main__':
+    flask_app.run(
+        host='127.0.0.1',
+        port=5010,
+        debug=True
+    )
+
